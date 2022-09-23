ec1e007344151e66fa86c040ba351a0d785d692e
alejandroriverohorrillo <alejandro.rivero.horrillo@ibm.com>
2022-09-23 12:05:31 +0200

    Lectura de la hoja de excel
    Co-authored-by: cgj-mcjb <cgj-mcjb@users.noreply.github.com>
    Co-authored-by: KaiLe-Ye <KaiLe-Ye@users.noreply.github.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import * as XLSX from "xlsx";
 
 function Home() {
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  //Reader of excel
+  const onChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    console.log("This file is: "+file);
+
+    reader.onload = (evt) => {
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: "binary" });
+      const wsname = wb.SheetNames[2];
+      const ws = wb.Sheets[wsname];
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      console.log("This data is: "+data);
+    };
+    reader.readAsBinaryString(file);
+  };
   return (
     <div className="home">
       <div class="container">
@@ -20,6 +39,12 @@ function Home() {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
+            <input
+              type="file"
+              accept=".xls,.xlsx"
+              value={selectedFile}
+              onChange={onChange}
+            />
           </div>
         </div>
       </div>
